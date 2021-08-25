02c8162e8d5b2d087249873575e163efd4786495
skf <conradmcgrifter@protonmail.com>
2021-08-25 00:38:40 -0400

    working on the typing bubbles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const LEFT1 = document.getElementById("left-1");
+const BUBBLE1 = document.querySelector(".typing-bubble");
 const LEFT2 = document.getElementById("left-2");
 const LEFT3 = document.getElementById("left-3");
 const DOG_PICS = document.getElementById("dog-pics");
@@ -11,11 +12,16 @@ const displayMessage = (element) => {
     element.classList.toggle("fadeIn");
 };
 
+const hideMessage = (element) => {
+    element.classList.toggle("hide");
+};
+
+setTimeout(hideMessage, 2000, BUBBLE1);
 setTimeout(displayMessage, 2000, LEFT1);
-setTimeout(displayMessage, 4000, LEFT2);
-setTimeout(displayMessage, 6000, DOG_PICS);
-setTimeout(displayMessage, 8000, RIGHT1);
-setTimeout(displayMessage, 9000, RIGHT2);
-setTimeout(displayMessage, 11000, LEFT3);
-setTimeout(displayMessage, 13000, PRICING1);
-setTimeout(displayMessage, 13000, PRICING2);
+// setTimeout(displayMessage, 4000, LEFT2);
+// setTimeout(displayMessage, 6000, DOG_PICS);
+// setTimeout(displayMessage, 6000, RIGHT1);
+// setTimeout(displayMessage, 8000, RIGHT2);
+// setTimeout(displayMessage, 11000, LEFT3);
+// setTimeout(displayMessage, 12000, PRICING1);
+// setTimeout(displayMessage, 12000, PRICING2);
